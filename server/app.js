0b80b90c932458e4d23fedefe44c12ade998b1a9
Swevosfr <naierabassi@gmail.com>
2023-01-09 23:23:05 +0100

    markous

--- server/app.js
+++ server/app.js
@@ -1,11 +1,26 @@
 // import modules
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 
 //app
+const app = express();
 
 // mongoose db
 
 //middleware
+app.use(morgan("dev"));
+app.use(cors({ origin: true, credentials: true }));
 
 //routes
 
 //port
+const port = process.env.PORT || 8080;
+
+//listener
+
+const server = app.listen(port, () =>
+  console.log(`Server is running on port ${port}`)
+);
